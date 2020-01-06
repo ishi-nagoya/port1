842ea947697696d8756f684f4817ce53e5a83a97
ishi-nagoya <56469394+ishi-nagoya@users.noreply.github.com>
2020-01-06 17:33:14 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -20,3 +20,16 @@
             });
         });
     });
+
+
+    $(function(){
+      $('a[href^="#"]').click(function(){
+        var adjust = 60;
+        var speed = 400;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top - adjust;
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+      });
+    });
